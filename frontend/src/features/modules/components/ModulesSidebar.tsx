e32eed7629f682786ac11193d71b251260eaeb50
TauzihKhan <travistowntopia@gmail.com>
2026-07-22 20:39:15 +0800

    fix - preserved dashboard sidebar compatibility

--- frontend/src/features/modules/components/ModulesSidebar.tsx
+++ frontend/src/features/modules/components/ModulesSidebar.tsx
@@ -23,7 +23,7 @@ export function ModulesSidebar({ user, onLogout }: ModulesSidebarProps) {
             }
             to="/dashboard"
           >
-            Need Help?
+            Dashboard
           </NavLink>
 
           <NavLink
@@ -59,7 +59,7 @@ export function ModulesSidebar({ user, onLogout }: ModulesSidebarProps) {
             }
             to="/topic-sheets"
           >
-            Topical Cheatsheets (TCs)
+            Topical Cheatsheets
           </NavLink>
 
           <NavLink
@@ -77,7 +77,7 @@ export function ModulesSidebar({ user, onLogout }: ModulesSidebarProps) {
             }
             to="/friends"
           >
-            My Friends
+            Friends
           </NavLink>
 
           <NavLink
@@ -86,7 +86,7 @@ export function ModulesSidebar({ user, onLogout }: ModulesSidebarProps) {
             }
             to="/sharing"
           >
-            Share Your TCs
+            Sharing
           </NavLink>
 
           <NavLink
@@ -104,7 +104,7 @@ export function ModulesSidebar({ user, onLogout }: ModulesSidebarProps) {
             }
             to="/shared-tcs"
           >
-            Your Received TCs
+            Shared TCs
           </NavLink>
         </nav>
       </div>
